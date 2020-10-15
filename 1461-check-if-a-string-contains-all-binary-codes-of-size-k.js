6f86063cb3bd1f86eb754ac81d4b9d903a740b0b
Celia Macrae <celiamacrae@gmail.com>
2020-10-15 10:20:14 -0700

    1461. Check If a String Contains All Binary Codes of Size K

---
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k.js
@@ -0,0 +1,17 @@
+const { expect } = require('chai');
+
+/*
+1461. Check If a String Contains All Binary Codes of Size K
+
+LEETCODE MEDIUM
+
+https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/
+
+Given a binary string s and an integer k.
+
+Return True if every binary code of length k is a substring of s. Otherwise, return False.
+*/
+
+var hasAllCodes = function(s, k) {
+    
+};
