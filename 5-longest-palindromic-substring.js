e99ddfba6ddb6433a85daea178c47d1a26ecc2fa
Celia Macrae <celiamacrae@gmail.com>
2020-10-15 09:27:50 -0700

    5. Longest Palindromic Substring

---
+++ 5-longest-palindromic-substring.js
@@ -0,0 +1,11 @@
+const { expect } = require('chai');
+
+/*
+5. Longest Palindromic Substring
+
+LEETCODE MEDIUM
+
+https://leetcode.com/problems/longest-palindromic-substring/
+
+Given a string s, return the longest palindromic substring in s.
+*/
