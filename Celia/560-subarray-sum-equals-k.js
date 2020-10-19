fec0c9d6c2745b86bcd2c889c0451c9bb0b143ba
Celia Macrae <celiamacrae@gmail.com>
2020-10-19 15:43:43 -0600

    added test case for 560

--- Celia/560-subarray-sum-equals-k.js
+++ Celia/560-subarray-sum-equals-k.js
@@ -9,3 +9,31 @@ https://leetcode.com/problems/subarray-sum-equals-k/
 
 Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
 */
+
+var subarraySum = function(nums, k) {
+    let sum = 0;
+    let count = 0;
+    
+    let hashMap = new Map()
+    hashMap.set(0, 1) //key = sum / val = count
+    
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i]
+        if (hashMap.has(sum - k)) {
+            count += hashMap.get(sum - k)
+        }
+
+        if (hashMap.has(sum)) {
+            hashMap.set(sum, hashMap.get(sum) + 1)
+        } else {
+            hashMap.set(sum, 1)
+        }
+    }
+    return count
+};
+
+describe('subarraySum', () => {
+    it('Test case 1', () => {
+        expect(subarraySum([1,1,1], 2)).to.equal(2);
+    });
+}); 
