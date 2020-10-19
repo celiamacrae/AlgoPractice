0e79e135958a5c79456d8f4a0ce1ea54c6d60bd1
Celia Macrae <celiamacrae@gmail.com>
2020-10-19 15:42:17 -0600

    560. Subarray Sum Equals K

---
+++ Celia/560-subarray-sum-equals-k.js
@@ -0,0 +1,11 @@
+const { expect } = require('chai');
+
+/*
+560. Subarray Sum Equals K
+
+LEETCODE MEDIUM
+
+https://leetcode.com/problems/subarray-sum-equals-k/
+
+Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+*/
