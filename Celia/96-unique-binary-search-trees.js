5b3d7196a72df75daf25e2118ee3a9a6bf897006
Celia Macrae <celiamacrae@gmail.com>
2020-10-21 11:49:01 -0600

    96- added more test cases and code passing all test cases

--- Celia/96-unique-binary-search-trees.js
+++ Celia/96-unique-binary-search-trees.js
@@ -24,11 +24,27 @@ Given n = 3, there are a total of 5 unique BST's:
 */
 
 var numTrees = function(n) { 
+    let dp = new Array(n+1).fill(0)
+    dp[0] = 1
+    dp[1] = 1
+    
+    for(let i=2; i<= n; i++){
+        for(let j=1; j<=i; j++){
+            dp[i] += dp[i - j] * dp[j -1]
+        }
+    }
 
-}
+    return dp[n]
+};
 
 describe('numTrees', () => {
     it('Test case 1', () => {
         expect(numTrees(3)).to.equal(5);
+    });
+    it('Test case 2', () => {
+        expect(numTrees(6)).to.equal(132);
+    });
+    it('Test case 3', () => {
+        expect(numTrees(10)).to.equal(16796);
     });
 }); 
