6c7f5564e6bc6644698d09416f22cc23afe80b35
Celia Macrae <celiamacrae@gmail.com>
2020-10-21 11:54:10 -0600

    1087. Brace Expansion

---
+++ Celia/1087-brace-expansion.js
@@ -0,0 +1,17 @@
+const { expect } = require('chai');
+
+/*
+1087. Brace Expansion
+
+LEETCODE MEDIUM
+
+https://leetcode.com/problems/brace-expansion/
+
+A string S represents a list of words.
+
+Each letter in the word has 1 or more options.  If there is one option, the letter is represented as is.  If there is more than one option, then curly braces delimit the options.  For example, "{a,b,c}" represents options ["a", "b", "c"].
+
+For example, "{a,b,c}d{e,f}" represents the list ["ade", "adf", "bde", "bdf", "cde", "cdf"].
+
+Return all words that can be formed in this manner, in lexicographical order.
+*/
