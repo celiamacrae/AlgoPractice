029cc0159402d2cce4361cee9641f24e37cf36c3
Celia Macrae <celiamacrae@gmail.com>
2020-10-23 13:31:58 -0600

    43 passes all test cases

--- Celia/43-multiply-strings.js
+++ Celia/43-multiply-strings.js
@@ -13,8 +13,37 @@ Note: You must not use any built-in BigInteger library or convert the inputs to 
 */
 
 var multiply = function(num1, num2) {
-
-}
+    let longer = num1.length > num2.length ? num1 : num2
+    let shorter = num2.length < num1.length ? num2 : num1
+    
+    if(longer === '0' || shorter ==='0'){
+        return '0'
+    }
+  
+    let carry = 0
+    let res = new Array(num1.length + num2.length).fill(0)
+  
+    for(let i=shorter.length -1; i>=0; i--){
+      for(let j=longer.length-1; j>= 0; j--){
+  
+        let curr = (shorter[i] * +longer[j]) + res[i+j+1]
+        let num = curr
+  
+        if(curr > 9){
+          num = curr % 10
+        }
+        res[i+j] += Math.floor(curr/10) 
+  
+        res[i+j+1] = num 
+      }
+    }
+  
+    let start = 0
+    while(res[start] === 0){
+      start ++
+    }
+    return res.slice(start).join('')
+  };
 
 describe('multiply', () => {
     it('Test case 1', () => {
