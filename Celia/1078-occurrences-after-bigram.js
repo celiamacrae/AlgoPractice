6e976350f1f89106fdfeaf8f67a7d78bb5a4ddc1
Celia Macrae <celiamacrae@gmail.com>
2020-10-23 11:50:56 -0600

    1078. Occurrences After Bigram

---
+++ Celia/1078-occurrences-after-bigram.js
@@ -0,0 +1,13 @@
+const { expect } = require('chai');
+
+/*
+1078. Occurrences After Bigram
+
+LEETCODE EASY
+
+https://leetcode.com/problems/occurrences-after-bigram/
+
+Given words first and second, consider occurrences in some text of the form "first second third", where second comes immediately after first, and third comes immediately after second.
+
+For each such occurrence, add "third" to the answer, and return the answer.
+*/
