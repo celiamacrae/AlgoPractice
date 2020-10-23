7e2b4f9923559e351339230dbcfa6e35a91e700e
Celia Macrae <celiamacrae@gmail.com>
2020-10-23 11:57:56 -0600

    1353. Maximum Number of Events That Can Be Attended

---
+++ Celia/1353-maximum-number-of-events-that-can-be-attended.js
@@ -0,0 +1,15 @@
+const { expect } = require('chai');
+
+/*
+1353. Maximum Number of Events That Can Be Attended
+
+LEETCODE MEDIUM
+
+https://leetcode.com/problems/maximum-number-of-events-that-can-be-attended/
+
+Given an array of events where events[i] = [startDayi, endDayi]. Every event i starts at startDayi and ends at endDayi.
+
+You can attend an event i at any day d where startTimei <= d <= endTimei. Notice that you can only attend one event at any time d.
+
+Return the maximum number of events you can attend.
+*/
