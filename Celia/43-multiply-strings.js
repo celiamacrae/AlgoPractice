beb042a6c166e41dacc6a8b4d7103c7bc6417657
Celia Macrae <celiamacrae@gmail.com>
2020-10-23 13:31:29 -0600

    43 added test cases

--- Celia/43-multiply-strings.js
+++ Celia/43-multiply-strings.js
@@ -11,3 +11,19 @@ Given two non-negative integers num1 and num2 represented as strings, return the
 
 Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
 */
+
+var multiply = function(num1, num2) {
+
+}
+
+describe('multiply', () => {
+    it('Test case 1', () => {
+        expect(multiply('2','3')).to.equal('6');
+    });
+    it('Test case 2', () => {
+        expect(multiply('123', '456')).to.equal('56088');
+    });
+    it('Test case 3', () => {
+        expect(multiply('0', '9')).to.equal('0');
+    });
+}); 
