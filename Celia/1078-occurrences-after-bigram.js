b6c868996904f32a64c86f434f9e0112b9c65716
Celia Macrae <celiamacrae@gmail.com>
2020-10-23 11:53:09 -0600

    1078 added test cases

--- Celia/1078-occurrences-after-bigram.js
+++ Celia/1078-occurrences-after-bigram.js
@@ -11,3 +11,16 @@ Given words first and second, consider occurrences in some text of the form "fir
 
 For each such occurrence, add "third" to the answer, and return the answer.
 */
+
+var findOcurrences = function(text, first, second) {
+
+}
+
+describe('findOcurrences', () => {
+    it('Test case 1', () => {
+        expect(findOcurrences("alice is a good girl she is a good student", "a", "good")).to.deep.equal(["girl","student"]);
+    });
+    it('Test case 2', () => {
+        expect(findOcurrences("we will we will rock you", "we", "will")).to.deep.equal(["we","rock"]);
+    });
+}); 
