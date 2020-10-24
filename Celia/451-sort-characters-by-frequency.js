54495b51a1e4b82977ae7a813f0d364b331ba525
Celia Macrae <celiamacrae@gmail.com>
2020-10-24 09:40:21 -0600

    451 added test cases

--- Celia/451-sort-characters-by-frequency.js
+++ Celia/451-sort-characters-by-frequency.js
@@ -11,5 +11,17 @@ Given a string, sort it in decreasing order based on the frequency of characters
 */
 
 var frequencySort = function(s) {
-    
+
 }
+
+describe('frequencySort', () => {
+    it('Test case 1', () => {
+        expect(frequencySort("tree")).to.equal("eert");
+    });
+    it('Test case 2', () => {
+        expect(frequencySort("cccaaa")).to.equal("cccaaa");
+    });
+    it('Test case 3', () => {
+        expect(frequencySort("Aabb")).to.equal("bbAa");
+    });
+}); 
