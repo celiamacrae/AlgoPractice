bfe5141ab0f4fbb255a428bc18a3c9e80207eaa6
Celia Macrae <celiamacrae@gmail.com>
2020-10-24 09:41:41 -0600

    451 added test cases

--- Celia/451-sort-characters-by-frequency.js
+++ Celia/451-sort-characters-by-frequency.js
@@ -11,12 +11,30 @@ Given a string, sort it in decreasing order based on the frequency of characters
 */
 
 var frequencySort = function(s) {
-
-}
+    let max = 0
+    let ht = {}
+    let newStr = ''
+    
+    for(let i=0; i<s.length; i++){
+        if(! ht.hasOwnProperty(s[i])){
+            ht[s[i]] = 0
+        }
+        ht[s[i]] ++
+        
+    }
+    
+    let letters = Object.keys(ht).sort((a ,b) => ht[b] - ht[a])
+    
+    for(let i=0; i<letters.length; i++){
+        newStr += letters[i].repeat(ht[letters[i]]) 
+    }
+    
+    return newStr
+};
 
 describe('frequencySort', () => {
     it('Test case 1', () => {
-        expect(frequencySort("tree")).to.equal("eert");
+        expect(frequencySort("tree")).to.equal("eetr");
     });
     it('Test case 2', () => {
         expect(frequencySort("cccaaa")).to.equal("cccaaa");
