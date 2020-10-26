f10290f291aa1db26ff2d012aca66d6c7915bcdd
Celia Macrae <celiamacrae@gmail.com>
2020-10-26 12:01:38 -0600

    349 added test cases

--- Celia/349-intersection-of-two-arrays.js
+++ Celia/349-intersection-of-two-arrays.js
@@ -9,3 +9,16 @@ https://leetcode.com/problems/intersection-of-two-arrays/
 
 Given two arrays, write a function to compute their intersection.
 */
+
+var intersection = function(nums1, nums2) {
+
+}
+
+describe('intersection', () => {
+    it('Test case 1', () => {
+        expect(intersection([1,2,2,1], [2,2])).to.deep.equal([2]);
+    });
+    it('Test case 2', () => {
+        expect(intersection([4,9,5], [9,4,9,8,4])).to.deep.equal([9,4]);
+    });
+}); 
