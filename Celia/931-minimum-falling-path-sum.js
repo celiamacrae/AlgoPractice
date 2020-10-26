ad9ad8b4e1a296ea7099882d0c6b83ea38b18c0a
Celia Macrae <celiamacrae@gmail.com>
2020-10-26 12:09:45 -0600

    931. Minimum Falling Path Sum

---
+++ Celia/931-minimum-falling-path-sum.js
@@ -0,0 +1,13 @@
+const { expect } = require('chai');
+
+/*
+931. Minimum Falling Path Sum
+
+LEETCODE MEDIUM
+
+https://leetcode.com/problems/minimum-falling-path-sum/
+
+Given a square array of integers A, we want the minimum sum of a falling path through A.
+
+A falling path starts at any element in the first row, and chooses one element from each row.  The next row's choice must be in a column that is different from the previous row's column by at most one.
+*/
