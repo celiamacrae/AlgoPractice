6e6169a154502d2b6986b97695e72209de78ff9d
Celia Macrae <celiamacrae@gmail.com>
2020-10-26 13:02:10 -0600

    1139. Largest 1-Bordered Square

---
+++ Celia/1139-largest-1-bordered-square.js
@@ -0,0 +1,11 @@
+const { expect } = require('chai');
+
+/*
+1139. Largest 1-Bordered Square
+
+LEETCODE MEDIUM
+
+https://leetcode.com/problems/largest-1-bordered-square/
+
+Given a 2D grid of 0s and 1s, return the number of elements in the largest square subgrid that has all 1s on its border, or 0 if such a subgrid doesn't exist in the grid.
+*/
