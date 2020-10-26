b456ad46e4cf7f875ab8709bd40580f8366bbe19
Celia Macrae <celiamacrae@gmail.com>
2020-10-26 11:57:13 -0600

    349. Intersection of Two Arrays

---
+++ Celia/349-intersection-of-two-arrays.js
@@ -0,0 +1,11 @@
+const { expect } = require('chai');
+
+/*
+349. Intersection of Two Arrays
+
+LEETCODE EASY
+
+https://leetcode.com/problems/intersection-of-two-arrays/
+
+Given two arrays, write a function to compute their intersection.
+*/
