172339b0abfedcf1a7a94d53b3eb66f021f29b52
Celia Macrae <celiamacrae@gmail.com>
2020-10-26 13:03:38 -0600

    1139 added test cases

--- Celia/1139-largest-1-bordered-square.js
+++ Celia/1139-largest-1-bordered-square.js
@@ -9,3 +9,16 @@ https://leetcode.com/problems/largest-1-bordered-square/
 
 Given a 2D grid of 0s and 1s, return the number of elements in the largest square subgrid that has all 1s on its border, or 0 if such a subgrid doesn't exist in the grid.
 */
+
+var largest1BorderedSquare = function(grid) {
+
+}
+
+describe('largest1BorderedSquare', () => {
+    it('Test case 1', () => {
+        expect(largest1BorderedSquare([[1,1,1],[1,0,1],[1,1,1]])).to.equal(9);
+    });
+    it('Test case 2', () => {
+        expect(largest1BorderedSquare([[1,1,0,0]])).to.equal(1);
+    });
+}); 
