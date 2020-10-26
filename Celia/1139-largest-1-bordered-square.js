a905d56fd8ef45da908a9cf97eb63c5c88b7a2b6
Celia Macrae <celiamacrae@gmail.com>
2020-10-26 13:06:49 -0600

    1139 passing all test cases

--- Celia/1139-largest-1-bordered-square.js
+++ Celia/1139-largest-1-bordered-square.js
@@ -11,7 +11,42 @@ Given a 2D grid of 0s and 1s, return the number of elements in the largest squar
 */
 
 var largest1BorderedSquare = function(grid) {
+    let x = grid.length 
+    let y = grid[0].length 
+    let max = Math.max(x, y)
+    
+    for(let i=0; i<max; i++){
+        let size = max - i
+        for(let j=0; j<x - size + 1; j++){
+            let startX = j
+            for(let k=0; k<y - size + 1; k++){
+                let startY = k
+                if(checkGrid(grid, startX, startY, size)){
+                    return size * size
+                }
+            }
+            
+        }
+    }
+    return 0  
+};
 
+function checkGrid(grid, startX, startY, size){
+    for(let i=0; i<size; i++){
+        if(grid[startX + i][startY] === 0){
+            return false
+        }
+        if(grid[startX][startY+i] === 0){
+            return false
+        }
+        if(grid[startX + i][startY + size -1] === 0){
+            return false
+        }
+        if(grid[startX + size -1][startY +i] === 0){
+            return false
+        }
+    }
+    return true 
 }
 
 describe('largest1BorderedSquare', () => {
