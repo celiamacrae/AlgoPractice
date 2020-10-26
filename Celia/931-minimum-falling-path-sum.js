9b956a806d21cd99a2340b0c667c0a1ced43ba6a
Celia Macrae <celiamacrae@gmail.com>
2020-10-26 12:11:10 -0600

    931 added test cases

--- Celia/931-minimum-falling-path-sum.js
+++ Celia/931-minimum-falling-path-sum.js
@@ -11,3 +11,13 @@ Given a square array of integers A, we want the minimum sum of a falling path th
 
 A falling path starts at any element in the first row, and chooses one element from each row.  The next row's choice must be in a column that is different from the previous row's column by at most one.
 */
+
+var minFallingPathSum = function(A) {
+
+}
+
+describe('minFallingPathSum', () => {
+    it('Test case 1', () => {
+        expect(minFallingPathSum([[1,2,3],[4,5,6],[7,8,9]])).to.equal(12);
+    });
+}); 
