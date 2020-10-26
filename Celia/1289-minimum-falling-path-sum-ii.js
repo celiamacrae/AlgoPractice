97647d3fa09780430245e62f14ff70c9b3ebef2a
Celia Macrae <celiamacrae@gmail.com>
2020-10-26 12:25:12 -0600

    1289. Minimum Falling Path Sum II

---
+++ Celia/1289-minimum-falling-path-sum-ii.js
@@ -0,0 +1,13 @@
+const { expect } = require('chai');
+
+/*
+1289. Minimum Falling Path Sum II
+
+LEETCODE HARD
+
+https://leetcode.com/problems/minimum-falling-path-sum-ii/
+
+Given a square grid of integers arr, a falling path with non-zero shifts is a choice of exactly one element from each row of arr, such that no two elements chosen in adjacent rows are in the same column.
+
+Return the minimum sum of a falling path with non-zero shifts.
+*/
