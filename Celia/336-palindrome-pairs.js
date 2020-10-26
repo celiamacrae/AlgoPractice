dd60bdf79343f13d9091f7fd21cccc6a80c7ba0c
Celia Macrae <celiamacrae@gmail.com>
2020-10-26 13:24:26 -0600

    336 added test cases

--- Celia/336-palindrome-pairs.js
+++ Celia/336-palindrome-pairs.js
@@ -9,3 +9,19 @@ https://leetcode.com/problems/palindrome-pairs/
 
 Given a list of unique words, return all the pairs of the distinct indices (i, j) in the given list, so that the concatenation of the two words words[i] + words[j] is a palindrome.
 */
+
+var palindromePairs = function(words) {
+
+}
+
+describe('minFallingPathSum', () => {
+    it('Test case 1', () => {
+        expect(palindromePairs(["abcd","dcba","lls","s","sssll"])).to.deep.equal([[0,1],[1,0],[3,2],[2,4]]);
+    });
+    it('Test case 2', () => {
+        expect(palindromePairs(["bat","tab","cat"])).to.deep.equal([[0,1],[1,0]]);
+    });
+    it('Test case 3', () => {
+        expect(palindromePairs(["a",""])).to.deep.equal([[0,1],[1,0]]);
+    });
+}); 
