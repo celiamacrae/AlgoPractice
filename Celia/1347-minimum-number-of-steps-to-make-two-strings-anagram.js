d6e7313d74d5957b647fc62e27749d3611d2fa8d
Celia Macrae <celiamacrae@gmail.com>
2020-10-26 18:26:39 -0600

    1347. Minimum Number of Steps to Make Two Strings Anagram

---
+++ Celia/1347-minimum-number-of-steps-to-make-two-strings-anagram.js
@@ -0,0 +1,15 @@
+const { expect } = require('chai');
+
+/*
+1347. Minimum Number of Steps to Make Two Strings Anagram
+
+LEETCODE MEDIUM
+
+https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram/
+
+Given two equal-size strings s and t. In one step you can choose any character of t and replace it with another character.
+
+Return the minimum number of steps to make t an anagram of s.
+
+An Anagram of a string is a string that contains the same characters with a different (or the same) ordering.
+*/
