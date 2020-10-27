92e26b7595881b2b0a2c7832dc5897caf4c0124f
Celia Macrae <celiamacrae@gmail.com>
2020-10-26 18:06:08 -0600

    1239 passing all test cases

--- Celia/1239-maximum-length-of-a-concatenated-string-with-unique-characters.js
+++ Celia/1239-maximum-length-of-a-concatenated-string-with-unique-characters.js
@@ -13,7 +13,35 @@ Return the maximum possible length of s.
 */
 
 var maxLength = function(arr) {
+    arr = arr.filter(word => uniqueLetters(word))
+    return checkCombos(arr) 
+};
 
+function checkCombos(arr){
+    let max = 0
+    words = new Set()
+    for(let i=0; i<arr.length; i++){
+        if(arr[i].length > max){
+            max = arr[i].length 
+        }
+        words.add(arr[i])
+        for(let key of words){ 
+            let newWord = key + arr[i]
+            if(uniqueLetters(newWord)){
+                
+                if(newWord.length > max){
+                    max = newWord.length
+                }
+                words.add(newWord)
+            }  
+        }
+    }
+    return max
+}
+
+function uniqueLetters(word){
+    let letters = new Set(word)
+    return word.length === letters.size
 }
 
 describe('maxLength', () => {
