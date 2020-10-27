bc208f8356c13b04eda825f9c474fe7a3ef79161
Celia Macrae <celiamacrae@gmail.com>
2020-10-26 18:28:49 -0600

    1347 added test cases

--- Celia/1347-minimum-number-of-steps-to-make-two-strings-anagram.js
+++ Celia/1347-minimum-number-of-steps-to-make-two-strings-anagram.js
@@ -13,3 +13,24 @@ Return the minimum number of steps to make t an anagram of s.
 
 An Anagram of a string is a string that contains the same characters with a different (or the same) ordering.
 */
+var minSteps = function(s, t) {
+
+}
+
+describe('minSteps', () => {
+    it('Test case 1', () => {
+        expect(minSteps("bab", "aba")).to.equal(1);
+    });
+    it('Test case 2', () => {
+        expect(minSteps("leetcode", "practice")).to.equal(5);
+    });
+    it('Test case 3', () => {
+        expect(minSteps("anagram", "mangaar")).to.equal(0);
+    });
+    it('Test case 4', () => {
+        expect(minSteps("xxyyzz", "xxyyzz")).to.equal(0);
+    });
+    it('Test case 5', () => {
+        expect(minSteps("friend", "family")).to.equal(4);
+    });
+}); 
