148a8f3e69fb70486e71389e0c793a891bdeeda6
Celia Macrae <celiamacrae@gmail.com>
2020-10-26 18:02:15 -0600

    1239. Maximum Length of a Concatenated String with Unique Characters

---
+++ Celia/1239-maximum-length-of-a-concatenated-string-with-unique-characters.js
@@ -0,0 +1,13 @@
+const { expect } = require('chai');
+
+/*
+1239. Maximum Length of a Concatenated String with Unique Characters
+
+LEETCODE MEDIUM
+
+https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/
+
+Given an array of strings arr. String s is a concatenation of a sub-sequence of arr which have unique characters.
+
+Return the maximum possible length of s.
+*/
