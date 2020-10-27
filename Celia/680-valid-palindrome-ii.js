c6cb210672ecb8f9d5b1db794163c7c0beb550eb
Celia Macrae <celiamacrae@gmail.com>
2020-10-27 13:01:03 -0600

    680 passing all test cases

--- Celia/680-valid-palindrome-ii.js
+++ Celia/680-valid-palindrome-ii.js
@@ -11,8 +11,37 @@ Given a non-empty string s, you may delete at most one character. Judge whether 
 */
 
 var validPalindrome = function(s) {
-
-}
+    let left = 0
+    let right = s.length -1
+    let skip = false
+     
+    while(left <= right){
+        if(s[left] !== s[right]){
+            if(skip === true){
+                return false
+            }
+            if(s[left] === s[right-1] && s[right] === s[left+1]){
+                left ++
+                right --
+            }
+            else if(s[left] === s[right-1]){
+                right = right -1
+                skip = true
+            }
+            else if(s[right] === s[left+1]){
+                left = left +1
+                skip = true
+            }else{
+                return false
+            } 
+        }else{
+            left ++
+            right --
+        }
+        
+    }
+    return true
+};
 
 describe('validPalindrome', () => {
     it('Test case 1', () => {
