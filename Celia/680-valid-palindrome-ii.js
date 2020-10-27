7dbd8b5e8414e955bb27c08543e36714fd77b74e
Celia Macrae <celiamacrae@gmail.com>
2020-10-27 13:00:23 -0600

    680 added test cases

--- Celia/680-valid-palindrome-ii.js
+++ Celia/680-valid-palindrome-ii.js
@@ -9,3 +9,16 @@ https://leetcode.com/problems/valid-palindrome-ii/
 
 Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
 */
+
+var validPalindrome = function(s) {
+
+}
+
+describe('validPalindrome', () => {
+    it('Test case 1', () => {
+        expect(validPalindrome("aba")).to.equal(true);
+    });
+    it('Test case 2', () => {
+        expect(validPalindrome("abca")).to.equal(true);
+    });
+}); 
