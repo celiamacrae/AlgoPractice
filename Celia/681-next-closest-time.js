ff2b71a29344f9a05a68f62fe177eb072bf70cd9
Celia Macrae <celiamacrae@gmail.com>
2020-10-28 11:53:00 -0600

    681 passing all test cases

--- Celia/681-next-closest-time.js
+++ Celia/681-next-closest-time.js
@@ -13,7 +13,57 @@ You may assume the given input string is always valid. For example, "01:34", "12
 */
 
 var nextClosestTime = function(time) {
-    
+    let diff = Infinity
+    let res = ''
+    let nums = new Set()
+    nums.add(+ time.substring(0,1))
+    nums.add(+ time.substring(1,2))
+    nums.add(+ time.substring(3,4))
+    nums.add(+ time.substring(4,5))
+
+    if(nums.size === 1){
+        return time
+    }
+
+    let digits = Array.from(nums)
+
+    let minutes = (+ time.substring(0,2)) * 60 + (+ time.substring(3,5))
+
+    dfs(digits, '', 0, minutes)
+
+    function dfs(digits, current, pos, target){
+        if(pos === 4){
+            let minute = (+ current.substring(0, 2) * 60 + (+ current.substring(2, 4)));
+            if(minute === target){
+                return
+            }
+            let d = minute - target > 0 ? minute - target : 1440 + minute - target
+            if(d < diff){
+                diff = d
+                result = current.substring(0, 2) + ':' + current.substring(2, 4);
+            }
+            return
+        }
+
+        for(let i=0; i<digits.length; i++){
+            if(pos === 0 && digits[i] > 2){
+                continue
+            }
+            if(pos === 1 && +current * 10 + digits[i] > 23){
+                continue
+            }
+            if(pos === 2 && digits[i] > 5){
+                continue
+            }
+            if(pos === 3 && +(current.substring(2)) * 10 + digits[i] > 59){
+                continue
+            }
+            dfs(digits, current + digits[i], pos + 1, target);
+        }
+
+    }
+
+    return result
 };
 
 describe('nextClosestTime', () => {
