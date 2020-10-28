b660da65aba790969951203e6ad93d0f662ac9aa
Celia Macrae <celiamacrae@gmail.com>
2020-10-28 11:37:48 -0600

    681. Next Closest Time

---
+++ Celia/681-next-closest-time.js
@@ -0,0 +1,17 @@
+const { expect } = require('chai');
+
+/*
+681. Next Closest Time
+
+LEETCODE MEDIUM
+
+https://leetcode.com/problems/next-closest-time/
+
+Given a time represented in the format "HH:MM", form the next closest time by reusing the current digits. There is no limit on how many times a digit can be reused.
+
+You may assume the given input string is always valid. For example, "01:34", "12:09" are all valid. "1:34", "12:9" are all invalid.
+*/
+
+var nextClosestTime = function(time) {
+    
+};
