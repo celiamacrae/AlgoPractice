18b177f650a4b14b4513f7432460387bcf098d95
Celia Macrae <celiamacrae@gmail.com>
2020-11-17 17:03:53 -0500

    1215. Stepping Numbers

---
+++ Celia/1215-stepping-numbers.js
@@ -0,0 +1,13 @@
+const { expect } = require('chai');
+
+/*
+1215. Stepping Numbers
+
+LEETCODE MEDIUM
+
+https://leetcode.com/problems/stepping-numbers/
+
+A Stepping Number is an integer such that all of its adjacent digits have an absolute difference of exactly 1. For example, 321 is a Stepping Number while 421 is not.
+
+Given two integers low and high, find and return a sorted list of all the Stepping Numbers in the range [low, high] inclusive.
+*/
