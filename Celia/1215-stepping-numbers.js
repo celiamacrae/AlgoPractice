bf262192790c6106cb1e1f893e6fd708e90c0b6f
Celia Macrae <celiamacrae@gmail.com>
2020-11-17 17:05:08 -0500

    1215 added test case

--- Celia/1215-stepping-numbers.js
+++ Celia/1215-stepping-numbers.js
@@ -11,3 +11,13 @@ A Stepping Number is an integer such that all of its adjacent digits have an abs
 
 Given two integers low and high, find and return a sorted list of all the Stepping Numbers in the range [low, high] inclusive.
 */
+
+var countSteppingNumbers = function(low, high) {
+
+}
+
+describe('countSteppingNumbers', () => {
+    it('Test case 1', () => {
+        expect(countSteppingNumbers(0, 21)).to.deep.equal([0,1,2,3,4,5,6,7,8,9,10,12,21]);
+    });
+}); 
